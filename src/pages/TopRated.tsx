77b79069bc334e00569bf63accc667cc497c274a
mariam <mariamsamyeg@gmail.com>
2024-07-17 13:14:52 +0300

    fixing

--- src/pages/TopRated.tsx
+++ src/pages/TopRated.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import Cover from '../components/Cover';
 import DisplayContainer from '../components/DisplayContainer';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store/store';
